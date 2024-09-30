68d49efa727e60e6358e0c0ca544d64953d53815
Nabina Purja <npurja173@gmail.com>
2024-09-30 10:39:05 +0100

    associations and validations added

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+  has_many :categories, through: :bookmarks
+  validates :description, :name, presence: true
+  validates :name, uniqueness: true
+  validates :rating, inclusion: { in: 0..10 }
 end
